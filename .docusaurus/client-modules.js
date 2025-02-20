f55b03735e4ac7981fbee1272367841e82e4a53e
Xylo Dan <dan@xylollc.com>
2025-02-20 14:06:21 -0800

    remove ignored files

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("C:\\Dev\\pyx-industries\\pyx-lamp\\pyx-resources-kb\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Dev\\pyx-industries\\pyx-lamp\\pyx-resources-kb\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Dev\\pyx-industries\\pyx-lamp\\pyx-resources-kb\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Dev\\pyx-industries\\pyx-lamp\\pyx-resources-kb\\src\\css\\custom.css"),
-  require("C:\\Dev\\pyx-industries\\pyx-lamp\\pyx-resources-kb\\node_modules\\@docusaurus\\plugin-google-gtag\\lib\\gtag"),
-];

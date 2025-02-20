de59d212e530c596325be8f22f409bf3b2fe0c9c
Xylo Dan <dan@xylollc.com>
2025-02-19 23:25:39 -0800

    update to deploy and .ignore

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,6 +1,7 @@
 export default [
-  require("C:\\Dev\\kb.pyx.io\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Dev\\kb.pyx.io\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Dev\\kb.pyx.io\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Dev\\kb.pyx.io\\src\\css\\custom.css"),
+  require("C:\\Dev\\pyx-industries\\pyx-lamp\\pyx-resources-kb\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Dev\\pyx-industries\\pyx-lamp\\pyx-resources-kb\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Dev\\pyx-industries\\pyx-lamp\\pyx-resources-kb\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Dev\\pyx-industries\\pyx-lamp\\pyx-resources-kb\\src\\css\\custom.css"),
+  require("C:\\Dev\\pyx-industries\\pyx-lamp\\pyx-resources-kb\\node_modules\\@docusaurus\\plugin-google-gtag\\lib\\gtag"),
 ];
